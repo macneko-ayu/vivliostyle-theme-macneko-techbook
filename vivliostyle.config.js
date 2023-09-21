6a14037ae4a66fdbc22810ef2b21cdb707812994
macneko-ayu <macneko.ayu@gmail.com>
2023-09-21 16:16:37 +0900

    change: change vivliostyle config

--- vivliostyle.config.js
+++ vivliostyle.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   language: 'en',
-  theme: ['node_modules/@vivliostyle/theme-base', '.'],
+  theme: [
+    'node_modules/@vivliostyle/theme-base', 
+    'node_modules/@vivliostyle/theme-techbook', 
+    '.'
+  ],
   entry: ['example/default.md'],
   workspaceDir: '.vivliostyle',
   output: [
